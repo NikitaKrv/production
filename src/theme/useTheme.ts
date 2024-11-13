36b40ad7723ff46a0915de82a28d64b51e303c0a
Nikita Kryuchkov <nikitakr.q@gmail.com>
2024-11-13 20:02:11 +0300

    initial commit

---
+++ src/theme/useTheme.ts
@@ -0,0 +1,21 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContest } from './ThemeContest'
+import { useContext } from 'react'
+
+interface UseThemeResult {
+	toggleTheme: () => void
+	theme: Theme
+}
+
+export function useTheme(): UseThemeResult {
+	const {theme, setTheme} = useContext(ThemeContest)
+	
+	const toggleTheme = () => {
+		const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+		localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+		setTheme(newTheme)
+	}
+	
+	return {
+		theme, toggleTheme
+	}
+}
