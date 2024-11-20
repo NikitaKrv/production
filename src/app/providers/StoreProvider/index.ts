2b87d666ecca2fcaee4c90131a58970024b1a395
Nikita Kryuchkov <nikitakr.q@gmail.com>
2024-11-20 15:17:03 +0300

    fix theme for modal

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,2 @@
+export { StoreProvider } from './ui/StoreProvider';
+export { createReduxStore } from 'app/providers/StoreProvider/config/store';
--- src/shared/config/tests/componentRender/componentRender.tsx
+++ src/shared/config/tests/componentRender/componentRender.tsx
@@ -2,22 +2,31 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { StoreProvider } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import i18nForTests from '../../i18n/i18nForTests';
 
 export interface componentRenderOptions {
 	route?: string
+  initialState?: DeepPartial<StateSchema>
 }
 
 export const componentRender = (
   component: ReactNode,
   options: componentRenderOptions = {},
 ) => {
-  const { route = '/' } = options;
+  const {
+    route = '/',
+    initialState,
+  } = options;
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests}>
-        { component }
-      </I18nextProvider>
-    </MemoryRouter>,
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTests}>
+          { component }
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>,
   );
 };
